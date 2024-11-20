c162bdc0d4ab364fbe41185e03e62ec2ac2b0fd4
Wish Marukapitak <97574512+wish-eq@users.noreply.github.com>
2024-11-20 23:57:13 +0700

    build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    FRONTEND_URL: process.env.FRONTEND_URL,
+    BACKEND_URL: process.env.BACKEND_URL,
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL
+  }
 };
 
 export default nextConfig;
